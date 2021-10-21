eda008d106ef29f95c5e21a019c061d3eba0f955
apjg1 <apjg1@pc8-040-l.cs.st-andrews.ac.uk>
2021-10-21 21:46:41 +0100

    Set up Single Webpage React app.

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { Navigation, Footer, Home, About, Contact} from "./components";
+
+function App() {
+  return (
+    <div className="App">
+		<Router>
+			<Navigation />
+			<Switch>
+				<Route path="/" exact component={() => <Home />} />
+				<Route path="/about" exact component={() => <About />} />
+				<Route path="/contact" exact component={() => <Contact />} />
+			</Switch>
+			<Footer />
+		</Router>
+    </div>
+  );
+}
+
+export default App;
