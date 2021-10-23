bff0adaf4bfa36d048561052bfa1d78781f6ae6e
UnikMask <alexandre.guerville@hotmail.com>
2021-10-23 15:52:44 +0100

    Added authentication testing outline.

---
+++ backend/authentication_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+// Test password hashing
+func testPwHash(t *testing.T) {
+
+}
+
+// Test password hash comparison
+func testPwComp(t *testing.T) {
+
+}
+
+// test user registration.
+func testRegisterUser(t *testing.T) {
+
+}
+
+// Test credential uniqueness with test database.
+func testCheckUnique(t *testing.T) {
+
+}
+
+// Test user sign-up using test database.
+func testSignUp(t *testing.T) {
+
+}
+
+// Test user log in.
+func testLogIn(t *testing.T) {
+
+}
