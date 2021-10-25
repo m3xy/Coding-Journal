1f5dde862d455c8f3e2b5a50b11fb88fae15982b
myyy1 <myyy1@st-andrews.ac.uk>
2021-10-25 13:51:19 +0000

    Delete teardownDB.sh

--- teardownDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-container="mariadb"
-docker stop $container
-
-#Note: this deletes all unused images also
-docker system prune --volumes -a -f
-
-# Use this if you don't want to delete the database volume
-# docker system prune
