d1f73c4cc0882bcb57ed4ee44c0571579ceb278f
ewp3 <ewp3@pc7-090-l.cs.st-andrews.ac.uk>
2021-11-06 19:55:50 +0000

    Commit to merge in db schema

---
+++ backend/CodeFiles.go
@@ -0,0 +1,13 @@
+/* 
+CodeFiles.go
+author: 190010425
+created: November 2, 2021
+
+This file handles reading/writing code files along with their
+data. The files are stored in the filesystem as follows:
+
+ProjectID (as stored in db Projects table)
+*/
+
+package main
+
---
+++ backend/CodeFiles_test.go
@@ -0,0 +1,7 @@
+/*
+CodeFiles_test.go
+author: 190010425
+created: November 2, 2021
+
+
+*/
