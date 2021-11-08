e41eb04cb9def27771c90173f55789d2c7b4bccf
apjg1 <apjg1@st-andrews.ac.uk>
2021-11-08 21:40:28 +0000

    Fixed ID mapping issue.

--- backend/db.go
+++ backend/db.go
@@ -13,7 +13,7 @@ var db *sql.DB
 const (
 	// Constant for table operations.
 	TABLE_USERS      = "users"
-	TABLE_IDMAPPINGS = "id_mappings"
+	TABLE_IDMAPPINGS = "idMappings"
 	TEAM_ID          = "11"
 	SELECT_ROW       = "SELECT %s FROM %s WHERE %s = ?"
 	INNER_JOIN       = "%s INNER JOIN %s"
