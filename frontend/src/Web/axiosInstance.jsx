a68b938352dbd1fb32cae400e52f112e42cb8727
UnikMask <alexandre.guerville@hotmail.com>
2022-01-28 13:19:20 +0000

    [frontend] Implemented interceptors for JWT.

--- frontend/src/Web/axiosInstance.jsx
+++ frontend/src/Web/axiosInstance.jsx
@@ -5,14 +5,30 @@
  * Author: 190014935
  */
 
-import axios from 'axios'
+import axios from "axios";
+import JwtService from "./jwt.service";
 
 // Axios instance used for making needed requests.
 const axiosInstance = axios.create({
   baseURL: process.env.BACKEND_ADDRESS,
   timeout: 1000,
-  headers: {"X-FOREIGNJOURNAL-SECURITY-TOKEN": process.env.BACKEND_TOKEN,
-            "Content-Type": "application/json"}
+  headers: {
+    "X-FOREIGNJOURNAL-SECURITY-TOKEN": process.env.BACKEND_TOKEN,
+    "Content-Type": "application/json",
+  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = JwtService.getAccessToken();
+    if (token) {
+      config.headers["bearer_token"] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
