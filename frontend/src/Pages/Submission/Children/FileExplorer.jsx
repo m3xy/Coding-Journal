4ec5e50d03f6d5ad1c752c7af7343f513a59dc13
apjg1 <apjg1@st-andrews.ac.uk>
2022-03-23 17:46:33 +0000

    [frontend] Added file browser.

---
+++ frontend/src/Pages/Submission/Children/FileExplorer.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react"
+import FileBrowser, { Icons } from "react-keyed-file-browser"
+import { Card } from "react-bootstrap"
+import "font-awesome/css/font-awesome.min.css"
+
+export default ({ files, onClick }) => {
+	const [fileArray, setFiles] = useState([])
+
+	useEffect(() => {
+		fileArrayToKeyedStruct(files)
+	}, [files])
+
+	// Get a file array of correct format from the files given by the parent structure.
+	const fileArrayToKeyedStruct = (array) => {
+		if (array !== undefined) {
+			let struct = []
+			array.map((file) => {
+				if (file.path.slice(-1) !== "/")
+					// Check if given file is not a directory.
+					struct = [
+						...struct,
+						{
+							key: file.path,
+							fileId: file.ID
+						}
+					]
+			})
+			setFiles(struct)
+		}
+	}
+
+	return (
+		<Card>
+			<Card.Body>
+				<h4>File Explorer</h4>
+				<FileBrowser
+					files={fileArray}
+					icons={Icons.FontAwesome(4)}
+					onSelect={(file) => {
+						if (file !== undefined) onClick(file.fileId)
+					}}
+					canFilter={false}
+					detailRenderer={() => null}
+				/>
+			</Card.Body>
+		</Card>
+	)
+}
