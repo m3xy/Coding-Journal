93516dec50b6d7f86ae0f8e6c9217b46779b8092
apjg1 <apjg1@st-andrews.ac.uk>
2022-03-23 12:00:42 +0000

    [backend][supergroup] Separated schemas to own file

--- backend/db.go
+++ backend/db.go
@@ -146,52 +146,6 @@ type Category struct {
 	DeletedAt time.Time `json:"-"`
 }
 
-// ---- Supergroup Data Structures ----
-
-// Supergroup compliant user type
-type SupergroupUser struct {
-	GlobalUserID string `json:"id"`
-	Email        string `json:"email"`
-	FirstName    string `json:"firstName"`
-	LastName     string `json:"lastName"`
-	PhoneNumber  string `json:"phoneNumber"`
-	Organization string `json:"organization"`
-}
-
-// Supergroup compliant code submissions (never stored in db)
-type SupergroupSubmission struct {
-	Name         string                   `json:"name"`
-	MetaData     SupergroupSubmissionData `json:"metadata"`
-	CodeVersions []SupergroupCodeVersion  `json:"codeVersions"`
-}
-
-// supergroup compliant structure for meta-data of the submission
-type SupergroupSubmissionData struct {
-	CreationDate time.Time `json:"creationDate"`
-	Abstract     string    `json:"abstract"`
-	License      string    `json:"license"`
-
-	Categories []string           `json:"categories"`
-	Authors    []SuperGroupAuthor `json:"authors"`
-}
-
-type SuperGroupAuthor struct {
-	ID      string `json:"userId"`
-	Journal string `json:"journal"`
-}
-
-// struct to store a supergroup compliant version of a submission
-type SupergroupCodeVersion struct {
-	TimeStamp time.Time        `json:"timestamp"`
-	Files     []SupergroupFile `json:"files"`
-}
-
-// Supergroup compliant file structure (never stored in db)
-type SupergroupFile struct {
-	Name        string `json:"filename"` // actually a file path not basename
-	Base64Value string `json:"base64Value"`
-}
-
 // ---- Database and reflect utilities ----
 
 // Initialise database - open connection, migrate tables, set logger.
