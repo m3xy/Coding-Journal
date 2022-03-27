a4c899a5ca8195d6057f3400556daebed3a9e3c7
apjg1 <apjg1@st-andrews.ac.uk>
2022-03-27 17:30:13 +0100

    [frontend] Removed all Navigation file.

--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -1,4 +1,3 @@
-export { default as Navigation } from "./Navigation"
 export { default as Footer } from "./Footer"
 export { default as Home } from "./HomePage/HomePage"
 export { default as About } from "./About"
