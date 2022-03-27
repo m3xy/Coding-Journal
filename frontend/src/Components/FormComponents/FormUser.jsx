c5032111c31bd11ee97bc23bfc7cd97b097124a5
apjg1 <apjg1@st-andrews.ac.uk>
2022-03-27 17:49:47 +0100

    [frontend] Added table to user form component.

--- frontend/src/Components/FormComponents/FormUser.jsx
+++ frontend/src/Components/FormComponents/FormUser.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react"
 import { Form, Table, Button, Dropdown, InputGroup } from "react-bootstrap"
 import axiosInstance from "../../Web/axiosInstance"
 
-export default ({ display, query, onChange }) => {
+export default ({ display, initUsers, query, onChange }) => {
 	const [array, setArray] = useState([])
 	const [input, setInput] = useState("")
 	const [results, setResults] = useState([])
@@ -23,7 +23,7 @@ export default ({ display, query, onChange }) => {
 						response.users !== undefined ? response.users : []
 					)
 				})
-				.catch((err) => {
+				.catch(() => {
 					return
 				})
 	}, [input])
@@ -32,12 +32,33 @@ export default ({ display, query, onChange }) => {
 		setInput(e.target.value)
 	}
 
+	// Handle a user selection.
 	const handleSelect = (user) => {
 		setArray((array) => {
 			return [...array, user]
 		})
 		setInput("")
 		val.current.value = ""
+	}
+
+	// Get a row entry for an user
+	const getUserRow = (user, key, options) => {
+		return (
+			<tr key={key}>
+				<td>{key}</td>
+				<td>
+					{user.firstName} {user.lastName}
+				</td>
+				<td>{user.profile?.email}</td>
+				<td>
+					{options ? (
+						options
+					) : (
+						<div className="text-muted">Cannot modify...</div>
+					)}
+				</td>
+			</tr>
+		)
 	}
 
 	return (
@@ -71,6 +92,39 @@ export default ({ display, query, onChange }) => {
 					})}
 				</Dropdown.Menu>
 			</Dropdown>
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Options</th>
+					</tr>
+				</thead>
+				<tbody>
+					{initUsers.map((user, i) => {
+						return getUserRow(user, i)
+					})}
+					{array.map((user, i) => {
+						return getUserRow(
+							user,
+							i + initUsers?.length,
+							<Button
+								variant="danger"
+								onClick={() => {
+									setArray((array) => {
+										return array.filter(
+											(check) =>
+												check.userId !== user.userId
+										)
+									})
+								}}>
+								Delete
+							</Button>
+						)
+					})}
+				</tbody>
+			</Table>
 		</Row>
 	)
 }
