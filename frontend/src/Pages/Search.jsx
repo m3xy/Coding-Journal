c42e02c89bf6dd5634a064f9233bafb377bb5232
myyy1 <myyy1@st-andrews.ac.uk>
2022-03-29 18:56:08 +0100

    [Fix] [Feature] Query:Reviewer/Author, Type, Tags

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -39,27 +39,39 @@ const defaultUserOrder = "firstName"
 function Search() {
 	const [search, setSearch] = useSearchParams()
 	const navigate = useNavigate()
+
 	const [submissions, setSubmissions] = useState([])
 	const [users, setUsers] = useState([])
-	const [showSubmissions, setShowSubmissions] = useState(false)
-	const [showUsers, setShowUsers] = useState(false)
-	const [submissionOrder, setSubmissionOrder] = useState(
-		defaultSubmissionOrder
-	)
-	const [userOrder, setUserOrder] = useState(defaultUserOrder)
+	const [submissionOrder, setSubmissionOrder] = useState(null)
+	const [userType, setUserType] = useState(null)
+	const [userOrder, setUserOrder] = useState(null)
 	const [tags, setTags] = useState([])
 	const [tagInput, setTagInput] = useState([])
+	const [authors, setAuthors] = useState([])
+	const [reviewers, setReviewers] = useState([])
 
 	useEffect(() => {
 		searchSubmissions()
 		searchUsers()
-	}, [search, submissionOrder, userOrder, tags])
+	}, [search, submissionOrder, userOrder, tags, userType, authors, reviewers])
 
 	const searchSubmissions = () => {
 		setSubmissions([])
+
+		const submissionSearch = new URLSearchParams(search)
+		submissionOrder && submissionSearch.append("orderBy", submissionOrder)
+		tags.forEach((tag) => {
+			submissionSearch.append("tags", tag)
+		})
+		authors.forEach((author) => {
+			submissionSearch.append("authors", author.userId)
+		})
+		reviewers.forEach((reviewer) => {
+			submissionSearch.append("reviewers", reviewer.userId)
+		})
 		axiosInstance
 			.get(submissionsQueryEndpoint, {
-				params: { name: search.get("name"), tags: tags, orderBy: submissionOrder }
+				params: submissionSearch
 			})
 			.then((response) => {
 				if (response.data.hasOwnProperty("submissions"))
@@ -86,9 +98,12 @@ function Search() {
 
 	const searchUsers = () => {
 		setUsers([])
+		const userSearch = new URLSearchParams(search)
+		userType && userSearch.append("userType", userType)
+		userOrder && userSearch.append("orderBy", userOrder)
 		axiosInstance
 			.get(usersQueryEndpoint, {
-				params: { name: search.get("name"), orderBy: userOrder }
+				params: userSearch
 			})
 			.then((response) => {
 				setUsers(response.data.users)
@@ -117,15 +132,38 @@ function Search() {
 					<Card.Subtitle className="mb-2 text-muted">
 						{user.profile.email}
 					</Card.Subtitle>
+					<Card.Text>{user.userId}</Card.Text>
 					<br />
-					<Button
-						variant="dark"
+					<Card.Link
 						size="sm"
 						onClick={() =>
 							navigate(profileURL + "/" + user.userId)
 						}>
 						View
-					</Button>
+					</Card.Link>
+
+					{!authors.some(author => (author.userId == user.userId)) && (
+						<Card.Link
+							size="sm"
+							onClick={() =>
+								setAuthors((authors) => [...authors, user])
+							}>
+							Add as author
+						</Card.Link>
+					)}
+
+					{!reviewers.some(reviewer => (reviewer.userId == user.userId)) && (
+						<Card.Link
+							size="sm"
+							onClick={() =>
+								setReviewers((reviewers) => [
+									...reviewers,
+									user
+								])
+							}>
+							Add as reviewer
+						</Card.Link>
+					)}
 				</Card.Body>
 				<Card.Footer>
 					<small className="text-muted">
@@ -136,14 +174,74 @@ function Search() {
 		)
 	})
 
-	const tagButtons = tags.map((tag, i) => {
+	const tagBtns = tags.map((tag, i) => {
 		return (
 			<Button
 				key={i}
-				variant="outline-secondary"
+				variant="outline-danger"
 				size="sm"
 				onClick={() => setTags(tags.filter((value) => value !== tag))}>
 				{tag}
+			</Button>
+		)
+	})
+
+	const cardContainer = (cards) => {
+		return (
+			<CardGroup>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						overflowX: "auto",
+						overflowY: "hidden"
+					}}>
+					{cards}
+				</div>
+			</CardGroup>
+		)
+	}
+
+	const authorBtns = authors.map((author) => {
+		return (
+			<Button
+				key={author.userId}
+				variant="outline-danger"
+				size="sm"
+				onClick={() =>
+					setAuthors(
+						authors.filter((elem) => elem.userId !== author.userId)
+					)
+				}>
+				{author.firstName +
+					" " +
+					author.lastName +
+					"(" +
+					author.profile.email +
+					")"}
+			</Button>
+		)
+	})
+
+	const reviewerBtns = reviewers.map((reviewer) => {
+		return (
+			<Button
+				key={reviewer.userId}
+				variant="outline-danger"
+				size="sm"
+				onClick={() =>
+					setReviewers(
+						reviewers.filter(
+							(elem) => elem.userId !== reviewer.userId
+						)
+					)
+				}>
+				{reviewer.firstName +
+					" " +
+					reviewer.lastName +
+					" (" +
+					reviewer.profile.email +
+					")"}
 			</Button>
 		)
 	})
@@ -153,25 +251,22 @@ function Search() {
 			<h3>Users:</h3>
 			Sort By:
 			<Form.Select onChange={(e) => setUserOrder(e.target.value)}>
-				<option default value="firstName">
-					First Name
-				</option>
+				<option default value={null}></option>
+				<option value="firstName">First Name</option>
 				<option value="lastName">Last Name</option>
+			</Form.Select>
+			User Type:
+			<Form.Select onChange={(e) => setUserType(e.target.value)}>
+				<option default value={null}></option>
+				<option value={0}>User</option>
+				<option value={1}>Publisher</option>
+				<option value={2}>Reviewer</option>
+				<option value={3}>Reviewer-Publisher</option>
+				<option value={4}>Editor</option>
 			</Form.Select>
 			<Row>
 				{users && users.length > 0 ? (
-					<CardGroup>
-						{" "}
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "row",
-								overflowX: "auto",
-								overflowY: "hidden"
-							}}>
-							{userCards}
-						</div>
-					</CardGroup>
+					cardContainer(userCards)
 				) : (
 					<div>No Users found</div>
 				)}
@@ -180,9 +275,8 @@ function Search() {
 			<h3>Submissions:</h3>
 			Sort By:
 			<Form.Select onChange={(e) => setSubmissionOrder(e.target.value)}>
-				<option default value="oldest">
-					Newest
-				</option>
+				<option default value={null}></option>
+				<option value="oldest">Newest</option>
 				<option value="newest">Oldest</option>
 				<option value="alphabetical">Alphabetical</option>
 			</Form.Select>
@@ -196,29 +290,22 @@ function Search() {
 					value={tagInput}
 				/>
 				<Button
-					variant="outline-secondary"
+					variant="outline-danger"
 					onClick={() => {
-						!tags.includes(tagInput) && setTags(tags => [...tags, tagInput])
+						!tags.includes(tagInput) &&
+							setTags((tags) => [...tags, tagInput])
 						setTagInput("")
 					}}>
 					Add
 				</Button>
 			</InputGroup>
-			{tagButtons}
+			{tagBtns}
+			<br />
+			{authors.length > 0 && <>Authors: {authorBtns} <br /></>}
+			{reviewers.length > 0 && <>Reviewers: {reviewerBtns} <br /></>}
 			<Row>
 				{submissions && submissions.length > 0 ? (
-					<CardGroup>
-						{" "}
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "row",
-								overflowX: "auto",
-								overflowY: "hidden"
-							}}>
-							{submissionCards}
-						</div>
-					</CardGroup>
+					cardContainer(submissionCards)
 				) : (
 					<div>No submissions found</div>
 				)}
